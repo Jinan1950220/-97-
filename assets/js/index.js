364fbc37caa0a70f355050aff06fa34fbb7c0e34
Jinan19950220 <920253667@qq.com>
2020-04-22 14:55:35 +0800

    完成后台用户信息渲染

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,36 @@ $(() => {
             layer(index);
         })
     })
+    //-------------------------获取用户信息---------------------
+    getUserInfo();
+
 })
+var getUserInfo = () => {
+    $.ajax({
+        type: 'GET',
+        url: 'http://www.liulongbin.top:3007/my/userinfo',
+        success: (res) => {
+            // console.log(res);
+            if (res.status === 0) {
+                //渲染页面
+                // 1.欢迎你 用户名（优先使用nickname 没有的花 使用username）
+                let name = res.data.nickname || res.data.username;
+                $('.welcome').html('欢迎&nbsp;&nbsp;' + name);
+                // 2.头像(优先使用图片 没有图片 优先使用那么)
+                if (res.data.user_pic) {
+                    $('.layui-nav-img').attr('src', res.data.user_pic);
+                    //让图片显示，让文字隐藏
+                    $('.layui-nav-img').show();
+                    $('.text-avatar').hide();
+                } else {
+                    let w = name.substr(0, 1).toUpperCase();
+                    $('.text-avatar').text(w).show().css('display', 'inline-block');
+                    $('.layui-nav-img').hide();
+                }
+            }
+        },
+        headers: {
+            Authorization: localStorage.getItem('token')
+        }
+    })
+}
